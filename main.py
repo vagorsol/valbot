9bee947099c29ae6f18355cf0005aaa632c8103d
Audrey Yang <audrey.butter0930@gmail.com>
2022-08-21 20:09:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,11 @@ intents.reactions = True
 
 bot = commands.Bot(intents = intents)
 
+@bot.event
+async def on_ready():
+    Channel = client.get_channel(CHANNEL_ID)
+
 # need on_raw_reaction_add and on_raw_reaction_remove
+@bot.event
+async def on_reaction_add(reaction, user):
+    # TODO
