ffff07a9f611a178b3f3b49e263191b9e86666aa
Audrey Yang <30783268+vagorsol@users.noreply.github.com>
2022-08-22 17:55:02 -0700

    Replaced awake message

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "*this bot is running* message"
+    return "Chirp! I am up and running!"
 
 def run():
     app.run(host='0.0.0.0', port = 8080)
